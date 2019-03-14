784892e07f51083b5478deb0da8a0e70575a9bf9
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2019-03-14 23:44:00 +0000

    fixed LDA for multiple class problem

--- LDA/main.m
+++ LDA/main.m
@@ -32,31 +32,26 @@ mu1 = mean(digit1);
 mu5 = mean(digit5);
 mu8 = mean(digit8);
 
-% Calculate verall mean
-% mu = (mu1 + mu5 + mu8)./3;
+% Calculate overall mean
+mu = (mu1 + mu5 + mu8)./3;
 
 % Calculate within class scatter matrix
 sw = cov(digit1) + cov(digit5) + cov(digit8);
-sw = sw + 0.00001*eye(size(sw));
 
-v = inv(sw) * (mu1 - mu5 - mu8)';
-ldaScore = data * v;
-
-% classSize = 100;
+% Add small numbers to avoid the inverse of zero
+sw = sw + 0.005*eye(size(sw));
 
 % Calculate between class scatter matrix
-% sb1 = classSize .* (mu1 - mu) * (mu1 - mu)';
-% sb5 = classSize .* (mu5 - mu) * (mu5 - mu)';
-% sb8 = classSize .* (mu8 - mu) * (mu8 - mu)';
-
-% sb = sb1 + sb5 + sb8;
+sb = cov([mu1;mu5;mu8]-mu);
 
 % Computing LDA projection
-% ldaProjection = sw / sb;
+ldaProjection = inv(sw) * sb;
 
-% [v,d] = eigs(ldaProjection);
+% Get eigenvectors and eigenvalues
+[v,d] = eigs(ldaProjection);
 
-% ldaScore = data*v(:,[1 2]);
+% Calculate LDA Score
+ldaScore = data*v(:,[1 2]);
 
 % Call function to perform k-means clustering
 [grid, clusterAssignments] = setGrid(ldaScore);
